42735d325462b318990d8e8031f41bd273d61133
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-01-20 17:42:22 -0600

    It works!

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-// (base: https://www.typescriptlang.org/play/)
+import { BoardConfig, ChessBoardInstance } from 'chessboardjs';
 
-class Greeter {
-  greeting: string;
-  constructor(message: string) {
-    this.greeting = message;
-  }
-  greet(): string {
-    return `Hello, ${this.greeting}`;
-  }
+declare const Chessboard: any;
+
+const config: BoardConfig = {
+  draggable: true,
+  dropOffBoard: 'snapback', // this is the default
+  onDrop: onDrop as any,
+  position: 'start'
+};
+
+function onDrop(source: any, target: any, piece: any, newPos: any, oldPos: any, orientation: any) {
+  console.log('Source: ' + source)
+  console.log('Target: ' + target)
+  console.log('Piece: ' + piece)
+  console.log('New position: ' + Chessboard.objToFen(newPos))
+  console.log('Old position: ' + Chessboard.objToFen(oldPos))
+  console.log('Orientation: ' + orientation)
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 }
 
-const greeter = new Greeter("world");
-
-const button = document.getElementById('myButton')!;
-button.onclick = () => {
-  alert(greeter.greet());
-};
+const board: ChessBoardInstance = Chessboard('myBoard', config)
