fff7aeb9d2549c21614c0042cd2b14b802f2bab7
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-01-20 19:33:24 -0600

    New approach

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { BoardConfig, ChessBoardInstance, Piece, PositionType, OrientationType } from 'chessboardjs';
 
-import { MoveList } from './move-list';
+import { Move , redrawMoveList} from './move-list';
 
 declare const Chessboard: any;
 
 
-let moveList: MoveList;
+const state = {
+  moves: [] as Move[],
+  selectedMove: undefined as number | undefined,
+};
+
+// let moveList: MoveList;
 const positionStack: string[] = [];
 let board: ChessBoardInstance;
 
 const config: BoardConfig = {
   draggable: true,
-  dropOffBoard: 'trash',
+  // dropOffBoard: 'trash',
   onDrop: onDrop as any,
   position: 'start'
 };
@@ -33,32 +38,39 @@ function onDrop(
     const piece = coloredPiece[1];
     const isCapture = Object.keys(newPos).length < Object.keys(oldPos).length;
 
-    moveList.add({
+    // TODO1 If in the past, don't append but rewrite history
+    state.moves.push({
       source,
       target,
       piece,
       isCapture,
     });
+    state.selectedMove = state.moves.length - 1;
+    redrawMoveList(state.moves, state.selectedMove);
+
     // moveList.select(moveList.moves.length - 1);
-  } else if (moveList.lastMove()) {
+  } else if (
+    // moveList.lastMove()
+    true
+  ) {
     console.log('TODO: Handle offboard');
     // const move = moveList.lastMove();
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const moveListElement = document.querySelector('#move-list');
-  if (!moveListElement) {
-    throw new Error('#move-list not found');
+document.addEventListener('keydown', (event: KeyboardEvent) => {
+  // TODO1 Implement a position stack and use it to update board
+  if (event.key === 'ArrowLeft') {
+    // TODO DRY ArrowRight
+    if (state.selectedMove !== undefined) {
+      state.selectedMove = Math.max(state.selectedMove - 1, 0)
+      redrawMoveList(state.moves, state.selectedMove);
+    }
+  } else if (event.key === 'ArrowRight') {
+    // TODO DRY ArrowLeft
+    if (state.selectedMove !== undefined) {
+      state.selectedMove = Math.min(state.selectedMove + 1, state.moves.length - 1)
+      redrawMoveList(state.moves, state.selectedMove);
+    }
   }
-  moveList = new MoveList(moveListElement as HTMLElement);
 });
-
-// document.querySelector('button#undo')!.addEventListener('click', () => {
-//   const position = positionStack.pop();
-//   if (!position) {
-//     return;
-//   }
-//   board.position(position, false);
-//   moveList.remove();
-// });
--- src/move-list.ts
+++ src/move-list.ts
@@ -7,65 +7,25 @@ export interface Move {
   isCapture?: boolean;
 }
 
-export class MoveList {
-  readonly moves: Move[] = [];
-  selectedMove: number | undefined;
-
-  constructor(private element: HTMLElement) {}
-
-  add(move: Move) {
-    this.moves.push(move);
-    this.updateDomForAdd();
+export function redrawMoveList(moves: Move[], selectedMove: number | undefined) {
+  const moveList = document.querySelector('#move-list');
+  if (!moveList) {
+    throw new Error('#move-list not found');
   }
 
-  // remove() {
-  //   if (this.moves.length === 0) {
-  //     throw new Error('MoveList.remove() called but there are no moves to remove');
-  //   }
-  //   this.moves.pop();
-  //   this.updateDomForRemove();
-  // }
+  moveList.innerHTML = '';
 
-  lastMove(): Move | undefined {
-    const move = this.moves.slice(-1)[0]; // TODO Add lib for .at()?
-    return move ? {...move} : undefined;
-  }
-
-  // select(moveIndex: number) {
-  //   this.selectedMove = moveIndex;
-  //   this.updateDomForSelect();
-  // }
-
-  // private updateDomForSelect() {
-  //   this.element.querySelectorAll('.selected-move').forEach(
-  //     (moveElement: Element) => {
-  //       moveElement.classList.remove('selected-move');
-  //     }
-  //   );
-  //   if (this.selectedMove !== undefined) {
-  //     this.element.children[this.selectedMove].classList.add('selected-move');
-  //   }
-  // }
-
-  private updateDomForAdd() {
-    const move = this.lastMove();
-    if (!move) {
-      return;
-    }
+  for (let [i, move] of moves.entries()) {
     const moveString = moveToString(move);
-    this.element.appendChild(htmlToElement(`
+    const moveElement = htmlToElement(`
       <div>${moveString}</div>
-    `));
+    `);
+    if (selectedMove === i) {
+      moveElement.classList.add('selected-move');
+    }
+    moveList.appendChild(moveElement);
   }
-
-  // private updateDomForRemove() {
-  //   if (this.element.lastChild) {
-  //     this.element.removeChild(this.element.lastChild);
-  //   }
-  // }
-
 }
-
 
 function moveToString(move: Move): string {
   return move.piece + (move.isCapture ? 'x' : '') + move.target;
